511c8902c892148cc27ed8226ca33730c96d96fd
Anukriti Jaiswal <68056558+WinexWolf@users.noreply.github.com>
2026-04-15 23:40:16 -0700

    add app tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Competitor Monitor</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
