4eefecc53cf61e76c6e8538c010944aa46c774f8
Anukriti Jaiswal <68056558+WinexWolf@users.noreply.github.com>
2026-04-16 22:46:47 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,11 @@
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
-import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Peekly</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
-      </Head>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
